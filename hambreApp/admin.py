4dd91b3326621a94b69fe08b975517346f43ba94
e-landeros <landerosedgard@gmail.com>
2018-12-29 22:15:02 -0800

    created static routes for rest.logos and created in admin

--- hambreApp/admin.py
+++ hambreApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from hambreApp.models import Restaurant
+
+admin.site.register(Restaurant)
--- hambre_app/urls.py
+++ hambre_app/urls.py
@@ -19,7 +19,8 @@ from hambreApp import views
 # dont need auth_views 
 # from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -37,4 +38,4 @@ urlpatterns = [
     path('restaurant/', include('django.contrib.auth.urls')),
     path('restaurant/', views.restaurant_home, name = 'restaurant-home'),
     path('restaurant/register/', views.restaurant_registration, name='resaturant-registration'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
