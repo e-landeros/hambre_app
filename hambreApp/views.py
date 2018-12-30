18d07a2223ae01099fcc110b62962fdb5e2cc06b
e-landeros <landerosedgard@gmail.com>
2018-12-29 17:51:32 -0800

    created registrationform for restuarants

--- hambreApp/views.py
+++ hambreApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from hambreApp.forms import UserForm, RestaurantForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -10,4 +13,10 @@ def restaurant_home(request):
     return render(request, 'restaurant/home.html', {})
 
 def restaurant_registration(request):
-    return render(request, 'restaurant/register.html', {})
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+
+    return render(request, 'restaurant/register.html', {
+        "user_form": user_form,
+        "restaurant_form":restaurant_form
+    })
