1163caf14cdf2b1e94850781d1ba27c8bdf3088c
e-landeros <landerosedgard@gmail.com>
2019-01-05 23:00:11 -0800

    restaurant dashboard sidebar design implementation

--- hambre_app/urls.py
+++ hambre_app/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('restaurant/sign-out', views.restaurant_sign_out, name='restaurant-sign-out'),
     path('restaurant/sign-up/', views.restaurant_sign_up, name= 'restaurant-sign-up'),
 
-    path('restaurant/', views.restaurant_home, name = 'restaurant-home'),
+    path('restaurant/', views.restaurant_home, name ='restaurant-home'),
     path('restaurant/account/', views.restaurant_account, name='restaurant-account'),
     path('restaurant/meal/', views.restaurant_meal, name='restaurant-meal'),
     path('restaurant/order/', views.restaurant_order, name='restaurant-order'),
